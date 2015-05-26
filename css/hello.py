9ab9ec8219b5a06a20b2773ddcc6e67cc7443117
Tilak Patidar <tilakpatidar@gmail.com>
2015-05-26 11:19:56 +0530

    Cleanup and merging with master

--- css/hello.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-print "Content-type:text/html\r\n\r\n"
-print "<html><head><title>Hello Word - First CGI Program</title></head><body><h2>Hello Word! This is my first CGI program</h2></body></html>"
-
